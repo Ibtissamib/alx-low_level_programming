937b8c1aa30ead19411c605347f4bd46816ee4fe
ibtissam <ibboutayeb@gmail.com>
2023-03-18 16:58:34 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,20 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 		for (j = 48; j <= 57; j++)
 			for (k = i; k <= 57; k++)
-				for (l = j + 1; l <= 57; l++)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(' ');
-					putchar(k);
-					putchar(l);
+				for (l = 48; l <= 57; l++)
+					if ((i != k) || (l > j))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
 					if ((i != 57) || (j != 56))
 					{
 						putchar (',');
 						putchar(' ');
 					}
-				}
+					}
 	putchar('\n');
 	return (0);
 }
