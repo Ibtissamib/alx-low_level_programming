ce456be62a02fda66c6890cd1a529cdbb5476a71
ibtissam <ibboutayeb@gmail.com>
2023-03-24 08:18:29 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,26 +12,17 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 15) == 0)
-		{
 			printf("%s", "FizzBuzz");
-			printf(" ");
-		}
 		else if ((i % 5) == 0)
-		{
 			printf("%s", "Buzz");
-			printf(" ");
-		}
 		else if ((i % 3) == 0)
-		{
 			printf("%s", "Fizz");
-			printf(" ");
-		}
 		else
-		{
 			printf("%d", i);
+		if (i == 100)
+			printf("\n");
+		else
 			printf(" ");
-		}
 	}
-	printf("\n");
-	return (0);
+		return (0);
 }
