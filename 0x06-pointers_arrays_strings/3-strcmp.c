e1c3ba9dec2e4fb59cca06958b5e485374a8c422
ibtissam <ibboutayeb@gmail.com>
2023-03-29 21:31:27 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,14 +13,19 @@ int _strcmp(char *s1, char *s2)
 
 	while ((*s1 != '\0') && (*s2 != '\0'))
 	{
-		s1++;
-		s2++;
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+			break;
 	}
-	if (*s1 != '\0')
-		n = 15;
-	else if (*s2 != '\0')
+	if (*s1 == *s2)
+		n = 0;
+	else if (*s1 < *s2)
 		n = -15;
 	else
-		n = 0;
+		n = +15;
 	return (n);
 }
