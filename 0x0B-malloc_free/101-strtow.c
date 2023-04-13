36f72128cf95d669cddc34d3e56c56834cdc9a85
ibtissam <ibboutayeb@gmail.com>
2023-04-13 14:59:36 -0700

    task6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,7 +18,7 @@ char **strtow(char *str)
 	nbr = nbr_wrd(str);
 	if (nbr == 0)
 		return (NULL);
-	p = malloc(nbr * sizeof(**p));
+	p = malloc(nbr * sizeof(char *));
 	if (p == NULL)
 		return (NULL);
 	while (k < nbr)
