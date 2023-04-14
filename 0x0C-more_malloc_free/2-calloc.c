7d9cb4c06db289435d9ca3daea89282631df4af1
ibtissam <ibboutayeb@gmail.com>
2023-04-13 18:19:31 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,28 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * set_b- set elements of the pointer to b
+ * @s: pointer to char
+ * @b: char
+ * @n: size of s
+ *
+ * Return: pointer
+ */
+char *set_b(char *s, char b, unsigned int n)
+{
+	char *set = s;
+
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+	return (set);
+}
+/**
  * _calloc- allocates memory for an array, using malloc
  * @nmemb: unsigned int
  * @size: unsigned int
+ * @n size of s
  *
  * Return: pointer tto the allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
-	int *s;
-	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(sizeof(int) * nmemb);
+	p = malloc(size * nmemb);
 	if (p == NULL)
 		return (NULL);
-	s = p;
-	while (i < nmemb)
-	{
-		s[i] = 0;
-	}
-	return (s);
-
+	set_b(p, 0, size * nmemb);
+	return (p);
 }
