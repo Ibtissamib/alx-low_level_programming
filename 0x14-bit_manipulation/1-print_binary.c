46b5e26be3c9b332ddd456956a5ebfc613d0bb22
Ibtissamib <ibboutayeb@gmail.com>
2023-05-04 14:18:40 -0700

    1. 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,22 +9,24 @@ void print_binary(unsigned long int n)
 {
 	unsigned int p = 1;
 
-	if (n == 0)
-		_putchar('0');
-	while (n >= p)
-		p = p * 2;
-	p = p / 2;
-	while (n >= 2)
-	{
-		n = n - p;
-		_putchar('1');
+	if (n == 0 || n == 1)
+		_putchar(n + '0');
+	else
+	{	while (n >= p)
+			p = p * 2;
 		p = p / 2;
-		while (n < p)
+		while (n >= 2)
 		{
+			n = n - p;
+			_putchar('1');
 			p = p / 2;
-			_putchar('0');
+			while (n < p)
+			{
+				p = p / 2;
+				_putchar('0');
+			}
 		}
+		if (n == 1)
+			_putchar('1');
 	}
-	if (n == 1)
-		_putchar('1');
 }
