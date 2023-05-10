3b254ca11e244b2effb8a80863d3628c6ea1e84b
Ibtissamib <ibboutayeb@gmail.com>
2023-05-10 14:53:02 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,8 +10,9 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t n = 0, len = 0;
 
-	while (text_content[len])
-		len++;
+	if (text_content != NULL)
+		while (text_content[len])
+			len++;
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
