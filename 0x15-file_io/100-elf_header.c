71d7b1e8743afa90e506fb40f311cafd0a9ef64c
Ibtissamib <ibboutayeb@gmail.com>
2023-05-14 10:04:36 -0700

    advanced

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -59,6 +59,9 @@ void print_version(char c)
 	printf("  Version:                           ");
 	switch (c)
 	{
+		case 0:
+			printf("Invalid");
+			break;
 		case 1:
 			printf("1 (current)");
 			break;
@@ -71,58 +74,43 @@ void print_version(char c)
 /**
  * print_OSABI- prints
  * @c: char
- *
  * Return: void
  */
-
 void print_OSABI(char c)
-{	printf("  OS/ABI:                            ");
-	switch (c)
-	{	case 0:
-			printf("UNIX - System V");
-			break;
-		case 1:
-			printf("Hewlett-Packard HP-UX");
-			break;
-		case 2:
-			printf("UNIX - NetBSD");
-			break;
-		case 3:
-			printf("Linux");
-			break;
-		case 6:
-			printf("UNIX - Solaris");
-			break;
-		case 7:
-			printf("AIX");
-			break;
-		case 8:
-			printf("IRIX");
-			break;
-		case 9:
-			printf("FreeBSD");
-			break;
-		case 10:
-			printf("Compaq TRU64 UNIX");
-			break;
-		case 11:
-			printf("Novell Modesto");
-			break;
-		case 12:
-			printf("Open BSD");
-			break;
-		case 13:
-			printf("Open VMS");
-			break;
-		case 14:
-			printf("Hewlett-Packard Non-Stop Kerne");
-			break;
-		default:
-			printf("<unknown: %x>", c);
-			break; }
-	printf("\n"); }
+{
+	printf("  OS/ABI:                            ");
+	if (c == 0)
+		printf("UNIX - System V");
+	else if (c == 1)
+		printf("Hewlett-Packard HP-UX");
+	else if (c == 2)
+		printf("UNIX - NetBSD");
+	else if (c == 3)
+		printf("Linux");
+	else if (c == 6)
+		printf("UNIX - Solaris");
+	else if (c == 7)
+		printf("AIX");
+	else if (c == 8)
+		printf("IRIX");
+	else if (c == 9)
+		printf("FreeBSD");
+	else if (c == 10)
+		printf("Compaq TRU64 UNIX");
+	else if (c == 11)
+		printf("Novell Modesto");
+	else if (c == 12)
+		printf("Open BSD");
+	else if (c == 13)
+		printf("Open VMS");
+	else if (c == 14)
+		printf("Hewlett-Packard Non-Stop Kerne");
+	else
+		printf("<unknown: %x>", c);
+	printf("\n");
+}
 /**
- * print_ABI- prints
+ * print_ABI_version- prints
  * @c: char
  *
  * Return: void
@@ -131,6 +119,12 @@ void print_ABI_Version(char c)
 {
 	printf("  ABI Version:                       %x\n", c);
 }
+/**
+ * print_type- prints type
+ * @c: char
+ *
+ * Return: void
+ */
 void print_type(char c)
 {
 	printf("  Type:                              ");
@@ -145,7 +139,8 @@ void print_type(char c)
 		case 2:
 			printf("EXEC (Executable file)");
 			break;
-		case 3: printf("DYN (Shared object file)");
+		case 3:
+			printf("DYN (Shared object file)");
 			break;
 		case 4:
 			printf("Core file");
@@ -178,7 +173,8 @@ int main(int ac, char **av)
 		exit(98); }
 	printf("ELF Header:\n  Magic:   ");
 	for (i = 0; i < 16; i++)
-	{	if ((buf[i] < 9) && i != 15)
+	{
+		if (buf[i] < 9 && i != 15)
 			printf("0%x ", buf[i]);
 		else if ((buf[i] < 9) && i == 15)
 			printf("0%x\n", buf[i]);
@@ -198,13 +194,13 @@ int main(int ac, char **av)
 		j = i;
 		while (i >= 24)
 		{
-			if (i != j && buf[i] <= 15 )
+			if (i != j && buf[i] <= 15)
 				printf("0%x", buf[i]);
 			else
 				printf("%x", buf[i]);
 			i--;
 		}
-	} 
+	}
 	else if (buf[5] == 2)
 	{	print_type(buf[17]);
 		printf("  Entry point address:               0x");
@@ -215,12 +211,12 @@ int main(int ac, char **av)
 		while (i <= 27)
 		{
 			if (buf[i] <= 15 && i != j && buf[i] != 0)
-                                printf("0%x", buf[i]);
+				printf("0%x", buf[i]);
 			else if (buf[i] != 0)
 				printf("%x", buf[i]);
 		i++;
 		}
-       	}
+	}
 	printf("\n");
 	close(fd);
 	return (0);
