ffa1ce361d41be5247a574f4085ef48d5d3975e7
Ibtissam <ibboutayeb@gmail.com>
2024-06-03 18:20:20 +0000

    11

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,13 +7,27 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t n = 0;
+	size_t nbr = 0;
+	const listint_t *memory_addresses[1000];
+	int i = 0, j = 0;
+
 	if (head == NULL)
 		return (0);
 	while (head)
 	{
-		n++;
+		printf("[%p] %d\n",(const void *)head, head->n);
+		nbr++;
 		head = head->next;
+		memory_addresses[i] = head;
+		i++;
+		while (j < i)
+		{
+			if (memory_addresses[j] == head)
+				exit(98);
+			j++;
+
+		}
+		j = 0;
 	}
-	return (n);
+	return (nbr);
 }
