f5e5fdd4b29b3508521c4423cee343d01311e3d3
Ibtissam <ibboutayeb@gmail.com>
2024-06-04 01:06:14 +0000

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint- prints
+ * @h: pointer to const dlistint_t
+ *
+ * Return: NBR OF NODES
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nbr = 0;
+
+	while(h)
+	{
+		printf("%d\n", h->n);
+		nbr++;
+		h = h->next;
+	}
+	return (nbr);
+}
